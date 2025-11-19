d41ce3ab2444fc0cbd559b86780fe05f38f88ada
justbraf <dwaynecorey@gmail.com>
2025-11-19 16:07:02 -0400

    feat: stat bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
+import StatBar from "./components/StatBar"
 
 function App() {
   // declare interface for pokemon data using only the keys desired from the API response
   interface Poke {
+    id: number,
     name: string,
     abilities: Array<{
       ability: {
@@ -24,43 +26,31 @@ function App() {
       .then((data: Poke) => setPokemon(data))
   }, [])
 
-  const capName = (name: string): string => {
-    return name[0].toUpperCase() + name.slice(1)
-  }
-
-  // Variables:
-  //   Base: The base stat value obtained from the PokeAPI.
-  //   IV: Individual Value (0-31). You'll need to provide this.
-  //   EV: Effort Value (0-252 per stat, max 510 total). You'll need to provide this.
-  //   Level: The Pokémon's level (1-100). You'll need to provide this.
-  //   Nature: A multiplier based on the Pokémon's nature (1.1 for a beneficial nature, 0.9 for a hindering nature, 1.0 for a neutral nature). You'll need to provide this.
-  const calcHP = (baseStat: number, ivHP: number, evHP: number, level: number): number => {
-    return Math.floor(0.01 * (2 * baseStat + ivHP + Math.floor(0.25 * evHP)) * level) + level + 10
-  }
-
-  const calcStats = (baseStat: number, ivStat: number, evStat: number, level: number, nature: number): number => {
-    return (Math.floor(0.01 * (2 * baseStat + ivStat + Math.floor(0.25 * evStat)) * level) + 5) * nature
+  const capWord = (word: string): string => {
+    return word[0].toUpperCase() + word.slice(1)
   }
 
   return (
     <div className="flex flex-col items-center">
       {pokemon ?
         <>
-          <p className="text-7xl mb-6">{capName(pokemon.name)}</p>
+          <p className="text-7xl mb-6">{capWord(pokemon.name)}</p>
           <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
+          <p className="text-3xl font-underline">Stats</p>
+          {
+            pokemon.stats.map((elem, index: number) => {
+              return <StatBar key={pokemon.id + index} name={capWord(elem.stat.name)} value={elem.base_stat} />
+            })
+          }
           <p className="text-3xl font-underline">Abilities:</p>
           <ul className="list-disc pl-8">
             {
               pokemon.abilities.map((skill: { ability }) => {
-                return <li className="text-2xl">{capName(skill.ability.name)}</li>
+                return <li className="text-2xl">{capWord(skill.ability.name)}</li>
               })
 
             }
           </ul>
-
-          {
-            pokemon.
-          }
           <audio controls src={pokemon.cries.latest}></audio>
         </>
         :
