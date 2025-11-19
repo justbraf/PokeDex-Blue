3785286c1400bdcaa3e5a6cab513259bbf8bc688
justbraf <dwaynecorey@gmail.com>
2025-11-19 15:22:44 -0400

    feat: stat calculator funcs

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react"
 function App() {
   // declare interface for pokemon data using only the keys desired from the API response
   interface Poke {
-    name: string;
+    name: string,
     abilities: Array<{
       ability: {
-        name: string;
+        name: string
       }
-    }>;
+    }>
   }
   const [pokemon, setPokemon] = useState<Poke>()
 
@@ -24,20 +24,39 @@ function App() {
       .then((data: Poke) => setPokemon(data))
   }, [])
 
-  return (
-    <>
-      <p className="text-4xl">Name: {pokemon?.name}</p>
-      <p className="text-3xl font-underline">Abilities:</p>
-      <ul className="list-disc pl-8">
-        {
-          pokemon?.abilities.map((skill: unknown) => {
-            return <li className="text-2xl">{skill.ability.name}</li>
-          })
+  const capName = (name: string): string => {
+    return name[0].toUpperCase() + name.slice(1)
+  }
 
-        }
-      </ul>
-      <audio controls src={pokemon?.cries.latest}></audio>
-    </>
+  const calcHP = (baseStat: number, ivHP: number, evHP: number, level: number): number => {
+    return Math.floor(0.01 * (2 * baseStat + ivHP + Math.floor(0.25 * evHP)) * level) + level + 10
+  }
+
+  const calcStats = (baseStat: number, ivStat: number, evStat: number, level: number, nature: number): number => {
+    return (Math.floor(0.01 * (2 * baseStat + ivStat + Math.floor(0.25 * evStat)) * level) + 5) * nature
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      {pokemon ?
+        <>
+          <p className="text-7xl mb-6">{capName(pokemon.name)}</p>
+          <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
+          <p className="text-3xl font-underline">Abilities:</p>
+          <ul className="list-disc pl-8">
+            {
+              pokemon?.abilities.map((skill: { ability }) => {
+                return <li className="text-2xl">{capName(skill.ability.name)}</li>
+              })
+
+            }
+          </ul>
+          <audio controls src={pokemon.cries.latest}></audio>
+        </>
+        :
+        <h1 className="text-9xl">Fetching Data...</h1>
+      }
+    </div>
   )
 }
 
